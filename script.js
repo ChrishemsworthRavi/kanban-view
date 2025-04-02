8d94d77aa83885e4a937e8349f8deb6c704a4eeb
Chris Hemsworth <your-email@example.com>
2025-04-02 18:16:24 +0530

    done

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import dotenv from "dotenv"; // Import dotenv package
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+dotenv.config(); // Load environment variables
+
+const SUPABASE_URL = import.meta.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.SUPABASE_ANON_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
